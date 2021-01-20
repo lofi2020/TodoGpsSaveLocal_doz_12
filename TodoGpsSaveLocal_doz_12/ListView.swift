69954ddb2149080a0f063c236d58096bb35ce5d9
LF <deepluv2k4@yahoo.de>
2021-01-20 15:16:12 +0100

    4.Change

--- TodoGpsSaveLocal_doz_12/ListView.swift
+++ TodoGpsSaveLocal_doz_12/ListView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("4")
+        }
     }
 }
 
